9fd668f561faa3d9108ea0e375f5c1f4b121b91c
Furkan <furkan.demirci@live.nl>
2020-04-29 15:24:52 +0200

    correct docker compose hostnames

--- src/services/Fambook.AuthService/Fambook.AuthService.Services/Helpers/ConsumeRabbitMQHostedService.cs
+++ src/services/Fambook.AuthService/Fambook.AuthService.Services/Helpers/ConsumeRabbitMQHostedService.cs
@@ -32,7 +32,7 @@ namespace Fambook.AuthService.Services.Helpers
 
         private void InitRabbitMq()
         {
-            var factory = new ConnectionFactory { HostName = "192.168.0.60" };
+            var factory = new ConnectionFactory { HostName = "fambook-rabbitmq" };
 
             // create connection  
             _connection = factory.CreateConnection();
